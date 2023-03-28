782ab7c4e47cfb75bae1c1dda43e1e963322c50e
Ethan Straub <evstraub@gmail.com>
2023-03-28 18:19:52 -0400

    technical difficulties with the styling so I installed tailwind, also added the ability to read from the data base.

--- src/components /LogOut.js
+++ src/components /LogOut.js
@@ -1,20 +1,20 @@
-import { auth } from "../firebase"
+import React from "react";
+import { auth } from "../firebase";
 
-
-
-
+const style = {
+  button: `bg-gray-200 px-4 py-2 hover:bg-gray-100`,
+};
 
 const LogOut = () => {
-    const signOut =() => {
-        signOut(auth)
-    }
-
+  const signOut = () => {
+    signOut(auth);
+  };
 
   return (
-    <div className="logOut">
-        <button onClick={() => auth.signOut()}>Log Out</button>
-    </div>
-  )
-}
+    <button onClick={() => auth.signOut()} className={style.button}>
+      Logout
+    </button>
+  );
+};
 
-export default LogOut
+export default LogOut;
