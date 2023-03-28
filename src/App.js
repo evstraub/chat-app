108922baa3e8b674e5fe427584d2144df4897711
Ethan Straub <evstraub@gmail.com>
2023-03-28 18:39:26 -0400

    it works

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className={style.appContainer}>
       <section className="{style.sectionContainer}">
-        {/* Navbar */}
+        
         <Navbar />
         {user ? <Chat /> : null}
       </section>
